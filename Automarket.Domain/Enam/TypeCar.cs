a8386906f04ae1ef1e19b576df783f79d06d3791
tutneshta <tutneshta@yandex.ru>
2023-04-04 16:39:52 +0300

    Add Domain and Enam

---
+++ Automarket.Domain/Enam/TypeCar.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Automarket.Domain.Enam
+{
+    public enum TypeCar
+    {
+        [Display (Name = "Легковой автомобиль")]
+        PassengerCar = 0,
+        [Display (Name = "Седан")]
+        Sedan = 1,
+        [Display (Name = "Хэтчбэк")]
+        HathBack = 2,
+        [Display (Name = "Минивэн")]    
+        Minivan = 3,
+        [Display (Name = "Спортивная машина")]
+        SportCar = 4,
+        [Display (Name = "Внедорожник")]
+        Suv = 5,
+    }
+}
--- Automarket/Controllers/HomeController.cs
+++ Automarket/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Automarket.Domain.Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Automarket.Models;
